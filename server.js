4a427933bd03e5a5ee6a00ed174ede57e2216bfb
ManoLo2ManoLo <manuel_canas@aol.com>
2022-01-08 01:46:10 -0500

    added server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//app.use(require('/.routes'));
+
+app.listen(PORT, () => console.log(`🌍🌍🌍 Connected on localhost:${PORT} 🌍🌍🌍`));
+
+mongoose.connect(process.env.MONGODB_URI ||  'mongodb://localhost:27017/social-network-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
